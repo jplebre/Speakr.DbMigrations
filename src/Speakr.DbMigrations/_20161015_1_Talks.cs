99f1a47bd609bf37200ed6fc1c702712826fc5cf
jplebre <jplebre@gmail.com>
2016-10-16 23:10:41 +0100

    Migration for initial tables, plus test data

---
+++ src/Speakr.DbMigrations/_20161015_1_Talks.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace Speakr.DbMigrations
+{
+    [Migration(1)]
+    public class _20161015_1_Talks : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Talks")
+                .WithColumn("TalkID").AsInt64().PrimaryKey().Identity()
+                .WithColumn("TalkEasyAccessKey").AsString(32).NotNullable()
+                .WithColumn("TalkName").AsString()
+                .WithColumn("Topic").AsString()
+                .WithColumn("Description").AsString()
+                .WithColumn("SpeakerName").AsString()
+                .WithColumn("TalkCreationTime").AsDateTime().NotNullable()
+                .WithColumn("TalkStartTime").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Talks");
+        }
+    }
+}
---
+++ src/Speakr.DbMigrations/_20161015_2_Questionnaires.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Speakr.DbMigrations
+{
+    [Migration(2)]
+    public class _20161015_2_Questionnaires : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Questionnaires")
+                .WithColumn("QuestionnaireID").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Questionnaire").AsString(Int32.MaxValue);
+
+            Create.Table("Feedback")
+                .WithColumn("FeedbackId").AsInt64().NotNullable().PrimaryKey().Identity()
+                .WithColumn("TalkID").AsGuid().NotNullable()
+                .WithColumn("Answer").AsString(Int32.MaxValue);
+
+            Create.Column("QuestionnaireID").OnTable("Talks").AsInt64()
+                .ForeignKey("Questionnaires", "QuestionnaireID");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("QuestionnaireID").FromTable("Talks");
+
+            Delete.Table("Questionnaires");
+            Delete.Table("Feedback");
+        }
+    }
+}
